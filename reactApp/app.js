160eed6dd87d9e2a0f0f708722d5d08455c91c9c
pdelancy <pdelancy94@gmail.com>
2017-10-30 13:21:13 -0700

    stuff

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,11 +1,59 @@
+
 var React = require('react');
 var ReactDOM = require('react-dom');
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+  _onItalicClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+  }
+  _onUnderlineClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  }
+  render() {
+    return (
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <div>
+          <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+          <button onClick={this._onItalicClick.bind(this)}>Italic</button>
+          <button onClick={this._onUnderlineClick.bind(this)}>Underline</button>
+        </div>
+
+        <div style={{
+          borderStyle: 'solid',
+          borderWidth: 'medium',
+          borderColor: 'maroon',
+          padding: '10px',
+          height: '300px',
+          width: '400px'}}>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+          />
+        </div>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <MyEditor />,
+  document.getElementById('root')
+);
 /* This can check if your electron app can communicate with your backend */
 // fetch('http://localhost:3000')
 // .then(resp => resp.text())
 // .then(text => console.log(text))
 // .catch(err => {throw err})
 
-ReactDOM.render(<p>React lives!</p>,
-   document.getElementById('root'));
+// ReactDOM.render(<p>React lives!</p>,
+//    document.getElementById('root'));
