8a8b2c5cdf2df415e0a7b0b1b9b55c8761bdabdd
luchen01 <luchenpengnyc@gmail.com>
2017-10-30 13:22:46 -0700

    add buttons

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+import Container from './container';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
 /* This can check if your electron app can communicate with your backend */
 // fetch('http://localhost:3000')
@@ -7,5 +9,17 @@ var ReactDOM = require('react-dom');
 // .then(text => console.log(text))
 // .catch(err => {throw err})
 
-ReactDOM.render(<p>React lives!</p>,
-   document.getElementById('root'));
+class MyEditor extends React.Component {
+  render() {
+    return (
+      <div>
+        <Container  />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <MyEditor />,
+  document.getElementById('root')
+);
